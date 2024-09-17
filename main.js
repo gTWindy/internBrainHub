4a51ad97aae547fdb79e62cd49dd6d7b52329abb
Курмашев Георгий Владимирович <kurmashev@opvf.ru>
2024-09-17 14:53:47 +0300

    Добавление блоков и поиска дочерних объектов

--- main.js
+++ main.js
@@ -1,41 +1,108 @@
-let isVisible = true;  // Измените на false, если кнопки не должны отображаться
-
-function updateButtonVisibility() {
+function updateButtonVisibility(isNotVisible) {
     const leftButton = document.getElementById('left-button');
     const rightButton = document.getElementById('right-button');
 
-    if (!isVisible) {
-        leftButton.style.display = 'none';
-        rightButton.style.display = 'none';
-    } else {
+    if (isNotVisible) {
         leftButton.style.display = 'inline-block';
         rightButton.style.display = 'inline-block';
+    } else {
+        leftButton.style.display = 'none';
+        rightButton.style.display = 'none';
     }
 }
-
 // Вызов функции для обновления видимости кнопок
-updateButtonVisibility();
+updateButtonVisibility(false);
 
-
-function transformState(imgName)
+function findChilds(id)
 {
-    const pic = document.querySelector('.main-image');
+    return global.inputArray.filter(person => person.parent === id);
+}
+function findStates()
+{
+    return global.inputArray.filter(state => !state.parent);
+}
+
+const rightButton = document.getElementById('right-button');
+rightButton.addEventListener('click', goHome);
+
+function addPersonCard(element, div)
+{
+    const newDiv = document.createElement('div');
+    newDiv.className = 'block';
+    
+    const newImg = document.createElement('img');
+    newImg.src = 'images/' + element.image;
+    newImg.style.borderRadius = '50%'; /* Для округления картинки */
+    newImg.className = "state-image";
+    newImg.addEventListener('click', () => {
+        transformState(element);
+    });
+    newDiv.appendChild(newImg);
+    const newSpan = document.createElement('span');
+    newSpan.className = 'text';
+    newSpan.textContent = element.name;
+    newDiv.appendChild(newSpan);
+
+    div.appendChild(newDiv);
+}
+
+function goHome()
+{
+    updateButtonVisibility(false);
+    let pic = document.querySelector('.main-image');
     if (pic) {
         console.log(pic.src); // Выведет URL первой найденной картинки
     }
-    pic.src = 'images/'+imgName;
-    pic.style.borderRadius = '50%'; /* Для округления картинки */
-    pic.style.maxWidth = '50%';
+    pic.src = 'main/main-1024.png';
+
+    const div = document.getElementById('row');
+    // Удаление всех элементов из div с помощью innerHTML
+    div.innerHTML = "";
+    const states = findStates();;
+    states.forEach(element => {
+        addPersonCard(element, div);
+    });
+
 }
 
+function transformState(obj)
+{
+    updateButtonVisibility(true) //Добавляем кнопки сверху по краям
+    
+    if (!obj)
+    {
+        console.log('Не найден')
+        return;
+    }
 
-// Получаем элемент по ID
-const pic1 = document.getElementById(1);
-const pic2 = document.getElementById(2);
-const pic3 = document.getElementById(3);
-// Добавляем обработчик события клика
-pic1.addEventListener('click', ()=>{transformState('nilfgaard.jpg')});
-// Добавляем обработчик события клика
-pic2.addEventListener('click', ()=>{transformState('redania.jpg')});
-// Добавляем обработчик события клика
-pic3.addEventListener('click', ()=>{transformState('skellige.jpg')});
+    let pic = document.querySelector('.main-image');
+    if (pic) {
+        console.log(pic.src); // Выведет URL первой найденной картинки
+    }
+    pic.src = 'images/' + obj.image;
+    pic.style.borderRadius = '50%'; /* Для округления картинки */
+    pic.style.maxWidth = '50%';
+
+    const div = document.getElementById('row');
+    // Удаление всех элементов из div с помощью innerHTML
+    div.innerHTML = "";
+    const childs = findChilds(obj.id);
+    childs.forEach(element => {
+        addPersonCard(element, div);
+    });
+    
+}
+
+for (let i = 1; i <= 3; i++) {
+    const pic = document.getElementById(i);
+    const obj = global.inputArray.find((element, index, array) => {
+        return element.id === i;
+    });
+    if (obj) {
+        pic.addEventListener('click', () => {
+            transformState(obj);
+        });
+    } else {
+        console.error(`Element with id ${i} not found in global.inputArray`);
+    }
+}
